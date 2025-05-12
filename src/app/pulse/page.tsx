9cbc8ebe9b7f451cc579e705cdf15e17ffb6e333
nam2ee <nyj5404@yonsei.ac.kr>
2025-05-13 00:40:53 +0900

    remove like reply share

--- src/app/pulse/page.tsx
+++ src/app/pulse/page.tsx
@@ -550,26 +550,7 @@ export default function PulseCommunity() {
                 )}
                 
                 {/* 인터랙션 버튼 - 네온 효과 추가 */}
-                <div className="flex gap-4 mt-4">
-                  <button className="text-gray-400 text-sm hover:text-blue-400 transition-colors duration-300 flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                    </svg>
-                    Like
-                  </button>
-                  <button className="text-gray-400 text-sm hover:text-purple-400 transition-colors duration-300 flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
-                    </svg>
-                    Reply
-                  </button>
-                  <button className="text-gray-400 text-sm hover:text-blue-400 transition-colors duration-300 flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
-                    </svg>
-                    Share
-                  </button>
-                </div>
+              
               </div>
             ))}
           </div>
